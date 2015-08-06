af88faa1ff507bd021b547573f04174bf881e338
misapps263 <misapps263@gmail.com>
2015-08-06 11:36:58 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,3 +19,46 @@ function mediaError(e) {
     alert('Media Error');
     alert(JSON.stringify(e));
 }
+function play_sound(src){
+
+var path = window.location.pathname;
+
+    path = path.substr( path, path.length - 10 );
+
+    absolutePath='file://' + path;
+
+ 
+
+if (device.platform == 'Android'){
+
+ src='/android_asset/'+src;
+
+ }
+
+var media = new Media(src,
+
+            function onSuccess() {
+
+                // release the media resource once finished playing
+
+                media.release();
+
+                                                               
+
+            },
+
+            function onError(e){
+
+                console.log("error playing sound: " + JSON.stringify(e));
+
+                                                               
+
+            });
+
+media.setVolume('1.0');
+
+media.play();
+
+}
+
+play_sound('audio/sound.mp3');
